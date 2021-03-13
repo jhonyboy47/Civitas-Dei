d71d7ccc0f3328a0d8ce52e9f8283f10c0070725
Zabdiyel <ztan@mines.edu>
2021-03-13 11:49:10 -0700

    added search fragments to navgraph

--- app/src/main/java/com/csci448/malagon/civitasdei/ui/profile/ChurchProfileFragment.kt
+++ app/src/main/java/com/csci448/malagon/civitasdei/ui/profile/ChurchProfileFragment.kt
@@ -1,10 +1,12 @@
 package com.csci448.malagon.civitasdei.ui.profile
 
+import androidx.fragment.app.Fragment
+
 /**
  * Author: Zabdiyel Tan
  * March 13, 2021
  *
  * Fragment for the profile page of a church
  */
-class ChurchProfileFragment {
+class ChurchProfileFragment: Fragment() {
 }
--- app/src/main/java/com/csci448/malagon/civitasdei/ui/search/SearchFragment.kt
+++ app/src/main/java/com/csci448/malagon/civitasdei/ui/search/SearchFragment.kt
@@ -1,10 +1,12 @@
 package com.csci448.malagon.civitasdei.ui.search
 
+import androidx.fragment.app.Fragment
+
 /**
  * Author: Zabdiyel Tan
  * March 13, 2021
  *
  * Search fragment generated when user selects the search button
  */
-class SearchFragment {
+class SearchFragment: Fragment() {
 }
