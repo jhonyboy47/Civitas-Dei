f7a3473e6ecf1eb3a9b280764c060c6e9821ac94
Jeremiah Navarro <jeridmn@gmail.com>
2021-03-13 21:48:04 -0700

    Added AttendantProfileFragmentAdapter, and added Post class and PostHolder class

---
+++ app/src/main/java/com/csci448/malagon/civitasdei/ui/profile/PostHolder.kt
@@ -0,0 +1,18 @@
+package com.csci448.malagon.civitasdei.ui.profile
+
+import android.widget.ExpandableListView
+import androidx.recyclerview.widget.RecyclerView
+import com.csci448.malagon.civitasdei.data.Post
+import com.csci448.malagon.civitasdei.databinding.RecyclerViewItemBinding
+
+class PostHolder(val binding: RecyclerViewItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    private lateinit var post: Post
+
+    fun bind(post: Post, clickListener: (Post) -> Unit){
+        this.post = post
+        itemView.setOnClickListener {clickListener(this.post)}
+        binding.postTitleTextView.text = this.post.title
+        binding.actionTextView.text = this.post.action
+        binding.profileFeedTimeTextView.text = this.post.time.toString()
+    }
+}
